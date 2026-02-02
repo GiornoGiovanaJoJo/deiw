c3faab2fc6536415ee336a087e33154ddf173426
GiornoGiovanaJoJo <kbootovsk@gmail.com>
2026-02-02 23:26:57 +0300

    create_adminka_user command and run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ set -e
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput --clear
 python manage.py create_default_superuser || true
+python manage.py create_adminka_user || true
 exec gunicorn config.wsgi:application --bind "0.0.0.0:${PORT:-8000}" --workers 1 --threads 2
